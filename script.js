4fc4b1838ddad649cddc51beb34eeb5e4e9b8b86
nick <pereiranicolas713@gmail.com>
2021-04-15 19:38:11 -0300

    criando estado via js

--- script.js
+++ script.js
@@ -1,31 +1,39 @@
-const generatingState = document.querySelector('#estado');
-console.log(generatingState);
-let State = [
-  'Acre',
-  'Alagoas',
- 'Amapá',
- 'Amazonas',
-  'Bahia',
-  'Ceará',
-  'Distrito Federal',
-  'Espírito Santo',
-  'Goiás',
-  'Maranhão',
-  'Mato Grosso',
-  'Mato Grosso do Sul',
-  'Minas Gerais',
-  'Pará',
-  'Paraíba',
-  'Paraná',
-  'Pernambuco',
-  'Piauí',
-  'Rio de Janeiro',
-  'Rio Grande do Norte',
-  'Rio Grande do Sul',
-  'Rondônia',
-  'Roraima',
-  'Santa Catarina',
-  'São Paulo',
-  'Sergipe',
-  'Tocantins',
-]
+const BrazilianState = document.querySelector('#estado');
+function brazilianState (){
+  let State = [
+    'Acre',
+    'Alagoas',
+   'Amapá',
+   'Amazonas',
+    'Bahia',
+    'Ceará',
+    'Distrito Federal',
+    'Espírito Santo',
+    'Goiás',
+    'Maranhão',
+    'Mato Grosso',
+    'Mato Grosso do Sul',
+    'Minas Gerais',
+    'Pará',
+    'Paraíba',
+    'Paraná',
+    'Pernambuco',
+    'Piauí',
+    'Rio de Janeiro',
+    'Rio Grande do Norte',
+    'Rio Grande do Sul',
+    'Rondônia',
+    'Roraima',
+    'Santa Catarina',
+    'São Paulo',
+    'Sergipe',
+    'Tocantins',
+  ]
+  for(let index = 0; index < State.length; index += 1){
+    const state = document.createElement('option');
+    BrazilianState.appendChild(state).innerText = State[index];
+    BrazilianState.appendChild(state).value = State[index];
+  }
+}
+brazilianState();
+
