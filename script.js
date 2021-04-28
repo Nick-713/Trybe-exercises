88c6209a855133d4f71a87e029fc67e7937b85a7
nick <pereiranicolas713@gmail.com>
2021-04-27 21:30:57 -0300

    parte 1 completa

--- script.js
+++ script.js
@@ -33,14 +33,23 @@ const order = {
     total: 60,
   },
 };
+
 const customerInfo = (order) => {
-  return `Olá, ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, Telefone: ${order.phoneNumber}, R.${order.address.street}, Nº:${order.address.number}, AP:${order.address.apartment}`;
+  return `Olá, ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, Telefone: ${order.phoneNumber}, R.${order.address.street}, Nº:${order.address.number}, AP:${order.address.apartment}.`;
 };
 
 console.log(customerInfo(order));
 
 const orderModifier = (order) => {
-  // Adicione abaixo as informações necessárias.
+  // quero de retorno  "Olá Luiz Silva, o total do seu pedido de muzzarella,calabresa e Coca-Cola Zero é R$ 50,00."
+  const newClient = (order.name = "Luiz Silva");
+  const newValueForPizza = (order.payment.total = "50");
+  const pizzas = Object.keys(order.order.pizza);
+  const drinks = Object.values(order.order.drinks.coke)[0];
+  return `Olá ${newClient}, o total do seu pedido de ${pizzas} e ${drinks} é R$ ${newValueForPizza}.`;
 };
 
-orderModifier(order);
+console.log(orderModifier(order));
+
+console.log("Objeto completo");
+console.log(order);
