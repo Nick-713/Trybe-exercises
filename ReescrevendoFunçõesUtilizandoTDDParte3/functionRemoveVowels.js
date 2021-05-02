ca89b3d0644f76ff840bed9e927e802cf95b15d5
nick <pereiranicolas713@gmail.com>
2021-05-02 01:46:05 -0300

    implementacao : greetPeople e removeVowels

---
+++ ReescrevendoFunçõesUtilizandoTDDParte3/functionRemoveVowels.js
@@ -0,0 +1,29 @@
+const removeVowels = (word) => {
+  const characters = word.split("");
+  const results = [];
+  let counter = 0;
+
+  for (let index = 0; index < characters.length; index += 1) {
+    if (
+      characters[index] === "a" ||
+      characters[index] === "o" ||
+      characters[index] === "i" ||
+      characters[index] === "e" ||
+      characters[index] === "u"
+    ) {
+      counter += 1;
+      results.push(counter);
+    } else {
+      results.push(characters[index]);
+    }
+  }
+  return results.join("");
+};
+
+const parameter = "Dayane";
+const result = "D1y2n3";
+
+const assert = require("assert");
+assert.strictEqual(typeof removeVowels, "function");
+const output = removeVowels(parameter);
+assert.strictEqual(output, result);
