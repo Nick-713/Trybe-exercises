abb0fad617f8e385a54a0378610236062174e69c
nick <pereiranicolas713@gmail.com>
2021-05-05 16:15:58 -0300

    parte 1 check

--- introducao-a-higher-order-functions/script.js
+++ introducao-a-higher-order-functions/script.js
@@ -1,3 +1,5 @@
+//Crie uma função de primeira classe que retorna um objeto { nomeCompleto, email } de uma nova pessoa contratada. Passe sua função como parâmetro da HOF newEmployees para criar cada pessoa contratada em seu respctivo id . A sua função deve receber como parâmetro o nome completo da pessoa funcionária e a partir dele gerar automaticamente um email no formato nome_da_pessoa@trybe.com .
+
 const hiredPerson = (person) => {
   const email = person.toLowerCase().split(' ').join('_');
   return { person, email: `${email}@betrybe.com` };
@@ -18,6 +20,8 @@ const newEmployees = (hiredPerson) => {
 };
 
 console.log(newEmployees(hiredPerson));
+
+//Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou").
 
 const numberCheck = (myNumber, number) => myNumber === number;
 
