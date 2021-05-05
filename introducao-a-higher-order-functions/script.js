2958810bd3a15b1ebed3030a68b1374743d13237
nick <pereiranicolas713@gmail.com>
2021-05-05 20:53:08 -0300

    game actions simulator

--- introducao-a-higher-order-functions/script.js
+++ introducao-a-higher-order-functions/script.js
@@ -34,3 +34,79 @@ const numberdraw = (myNumber, callback) => {
 };
 
 console.log(numberdraw(5, numberCheck));
+
+/* 
+Crie uma função que retorna o dano do dragão.
+O dano será um número aleatório entre 15 (dano mínimo) e o valor do atributo strength (dano máximo). */
+
+const mage = {
+  healthPoints: 130,
+  intelligence: 45,
+  mana: 125,
+  damage: undefined,
+};
+
+const warrior = {
+  healthPoints: 200,
+  strength: 30,
+  weaponDmg: 2,
+  damage: undefined,
+};
+
+const dragon = {
+  healthPoints: 350,
+  strength: 50,
+  damage: undefined,
+};
+
+const battleMembers = { mage, warrior, dragon };
+
+/* Crie uma função que retorna o dano do dragão.
+O dano será um número aleatório entre 15 (dano mínimo) e o valor do atributo strength (dano máximo). */
+
+const dragonDamage = (dragon) => {
+  const minDamage = 15;
+  const strength = dragon.strength;
+  const dragonDm = Math.floor(
+    Math.random() * (strength - minDamage) + minDamage
+  );
+  return dragonDm;
+};
+
+console.log(dragonDamage());
+
+/* Crie uma função que reotorna o dano causado pelo warrior .
+  O dano será um número aleatório entre o valor do atributo strength (dano mínimo) e o valor de strength * weaponDmg (dano máximo). */
+
+const warriorDamage = () => {
+  const strength = warrior.strength;
+  const maxDm = warrior.strength * warrior.weaponDmg;
+  const warriorDm = Math.floor(Math.random() * (maxDm - strength) + strength);
+  return warriorDm;
+};
+
+console.log(warriorDamage());
+/* 
+  Crie uma função que retorna um objeto com duas chaves e dois valores contendo o dano e a mana gasta pelo mago em um turno.
+  O dano será um número aleatório entre o valor do atributo intelligence (dano mínimo) e o valor de intelligence * 2 (dano máximo).
+  A mana consumida por turno é 15. Além disto a função deve ter uma condicional, caso o mago tenha menos de 15 de mana o valor de dano recebe uma mensagem (Ex: "Não possui mana suficiente") e a mana gasta é 0. */
+
+const mageAttack = (mage) => {
+  const mageMana = mage.mana;
+  const minDmg = mage.intelligence;
+  const maxDmg = minDmg * 2;
+  const turnStats = {
+    manaSpent: 0,
+    damageDealt: 'Não possui mana suficiente',
+  };
+
+  if (mageMana > 15) {
+    const mageDamage = Math.floor(Math.random() * (maxDmg - minDmg) + minDmg);
+    turnStats.manaSpent = 15;
+    turnStats.damageDealt = mageDamage;
+    return turnStats;
+  }
+  return turnStats;
+};
+
+console.log(mageAttack(mage));
