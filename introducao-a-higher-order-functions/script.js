e1b9130669bf66678257b063639ebae6c7046717
nick <pereiranicolas713@gmail.com>
2021-05-06 22:02:13 -0300

    parte 2 - exercicio 1

--- introducao-a-higher-order-functions/script.js
+++ introducao-a-higher-order-functions/script.js
@@ -110,3 +110,14 @@ const mageAttack = (mage) => {
 };
 
 console.log(mageAttack(mage));
+
+//Crie a primeira HOF que compõe o objeto gameActions . Ela será a função que simula o turno do personagem warrior . Esta HOF receberá como parâmetro a função que calcula o dano deferido pelo personagem warrior e atualizará os healthPoints do monstro dragon . Além disto ela também deve atualizar o valor da chave damage do warrior .
+const gameActions = {
+  // Crie as HOFs neste objeto.
+  warriorTurn: (warriorDamage) => {
+    const attackWarrior = warriorDamage();
+    warrior.damage = attackWarrior;
+    dragon.healthPoints -= attackWarrior;
+  },
+};
+gameActions.warriorTurn(warriorDamage);
